6dc7659c4c1e236b08c23464c28767019030da8e
nealwaga <waganeal@gmail.com>
2022-10-10 19:36:39 +0300

    keywords

--- Statements (3)/main.js
+++ Statements (3)/main.js
@@ -7,3 +7,14 @@ c = a + b;
 
 // When separated by semicolons, multiple statements on one line are allowed:
 a = 5; b = 6; c = a + b;
+
+// Some JavaScript keywords:
+// var - declares a variable
+// let - declares a block variable
+// const - declares a block constant 
+// if - marks a block of statements to be executed on a condition 
+// switch - marks a block of statements to be executed in different cases
+// for - marks a block of statements to be executed in a loop
+// function - declares a function
+// return - exits a function
+// try - implements error handling to a block of statements
