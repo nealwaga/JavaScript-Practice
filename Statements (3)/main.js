bed39125f4ec28833cd878626ada47b7fed29167
nealwaga <waganeal@gmail.com>
2022-10-10 19:20:17 +0300

    semicolons

---
+++ Statements (3)/main.js
@@ -0,0 +1,9 @@
+// Semicolons separate JavaScript statements. Example:
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+
+
+// When separated by semicolons, multiple statements on one line are allowed:
+a = 5; b = 6; c = a + b;
