ac16a504b54e918b68ba150e3d59fe3c44444ea5
nealwaga <waganeal@gmail.com>
2022-10-11 17:41:49 +0300

    const arrays

--- Const (7)/main.js
+++ Const (7)/main.js
@@ -3,3 +3,12 @@
 // Variables defined with const have Block Scope.
 
 const PI = 3.14;
+
+// Constant Objects and Arrays
+// Constant Arrays
+// You can create a costant array:
+const cars = ["Audi", "BMW", "Mercedes"];
+// You can change an element
+cars[0] = "Rolls Royce";
+// You can add an element
+cars.push("Volvo");
