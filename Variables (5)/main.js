29ab26ebe8cd49d47925b960bb938c7a5a53f870
nealwaga <waganeal@gmail.com>
2022-10-11 16:40:14 +0300

    variables

---
+++ Variables (5)/main.js
@@ -0,0 +1,5 @@
+// Variables are containers for storing data (storing data values).
+// You can declare a JavaScript variable using 'var', 'let', 'const' or using nothing. Examples:
+var x = 5;
+var y = 6;
+var z = x + y;
