17de6f756c99c7f645208fbb2217aea285b04812
nealwaga <waganeal@gmail.com>
2023-06-22 09:46:36 +0300

    return statement

--- Statements (3)/Continue_Statements/script_1.js
+++ Statements (3)/Continue_Statements/script_1.js
@@ -1,4 +1,5 @@
-// The continue statement is used to break the iteration of the loop and follows with the next iteration. The break in the iteration is possible only when the specified condition going to occur.
+// The continue statement is used to break the iteration of the loop and follows with the next iteration. 
+// The break in the iteration is possible only when the specified condition going to occur.
 /// Example 1 - continue statement in a for loop
 for (let i = 0; i < 11; i++) {
     if (i % 2 == 0)continue;
